38ffdca1d6ba8fb487b259397d3c5b4b6127c5ec
Iriazul <iriazuliriazul029@gmail.com>
2016-08-29 00:37:19 +0200

    Setup

---
+++ Modular-Weaponry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Modular_Weaponry
+{
+    public class Modular_Weaponry : Mod
+    {
+        public Modular_Weaponry()
+        {
+            this.Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
