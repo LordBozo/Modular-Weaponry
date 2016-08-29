01b608945cac552f84e57d82dfaec684448cdc44
LordBozo <gerik.bennett@gmail.com>
2016-08-28 21:11:38 -0400

    Module
    nope, updates shall be frequent

---
+++ Module.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.UI;
+
+public delegate bool ApplyModule(ref Item weapon);
+public class Module : ModItem
+{
+    public int moduleType;
+    public ApplyModule applyModule;
+}
